748e59d2b645838b797dcd45251600ef0c25d6e8
Nick Walther <newalth@cs.utexas.edu>
2013-10-27 20:19:03 -0500

    Changed layout of Course Detail page to be a sortable table.

--- mygrades/static/javascript/course-detail.js
+++ mygrades/static/javascript/course-detail.js
@@ -1,57 +1,45 @@
-// sorting of both the course list and assignment lists
-$(function() {
-    var sort = $('#sortable-courselist');
-    sort.sortable({
-      	placeholder: 'course placeholder'
+// sorting of the data in the assignments table and categories table
+$(function(){
+    var assignmentsTable = $("#assignments-table")
+    var categoriesTable = $("#categories-table")
+
+    assignmentsTable.tablesorter({
+        sortReset: true,
+
+        emptyTo: 'bottom',
+
+        // on page load, sort on the sixth column and first column in ascending order
+        sortList: [[5,0],[0,0]],
+
+        sortInitialOrder: 'asc',
+        headers : {
+            2 : { sortInitialOrder: 'desc' }, // awarded points first click is most to least
+            3 : { sortInitialOrder: 'desc' }, // possible points first click is most to least
+            6: { sorter: false} // don't sort operations column
+        }
     });
-    sort.disableSelection();
 
-    var sort2 = $('.sortable-assignmentlist');
-    sort2.sortable({
-      	placeholder: 'assignment placeholder'
+    assignmentsTable.data("tablesorter").widgets = ["zebra"];
+    assignmentsTable.trigger('applyWidgets');
+
+    categoriesTable.tablesorter({
+        sortReset: true,
+
+        emptyTo: 'bottom',
+
+        // on page load, sort on the fifth column and first column in ascending order
+        sortList: [[4,0],[0,0]],
+
+        sortInitialOrder: 'asc',
+        headers : {
+            1 : { sortInitialOrder: 'desc' }, // awarded points first click is most to least
+            2 : { sortInitialOrder: 'desc' }, // possible points first click is most to least
+            5: { sorter: false} // don't sort operations column
+        }
     });
-    sort2.disableSelection();
-});
 
-// add course dialog box
-$(function() {
-	var number = $( "#number" );
-	var name = $( "#name" );
-	var instructor = $( "#instructor" );
-	//var allFields = $( [] ).add( number ).add( name ).add( instructor );
-	var tips = $( ".validate-tips" );
-
-	$( "#add-course-dialog-form" ).dialog({
-		autoOpen: false,
-		height: 325,
-		width: 375,
-		modal: true,
-		buttons: {
-			"Add course": function() {
-				var isValid = true;
-
-				// do stuff to check for validity of inputs
-
-		        if ( isValid ) {
-	      			// store data in database
-	      			$( this ).dialog( "close" );
-	    	  	} else {
-	    	  		// change validate-tips to help user type valid inputs
-	    	  	}
-	      	},
-		  	Cancel: function() {
-	  			$( this ).dialog( "close" );
-		  	}
-		},
-		close: function() {
-			// do special stuff when closing window
-			// such as prep for next time window is opened
-		}
-	});
-
-	$( ".add-course-button" ).button().click(function() {
-		$( "#add-course-dialog-form" ).dialog( "open" );
-	});
+    categoriesTable.data("tablesorter").widgets = ["zebra"];
+    categoriesTable.trigger('applyWidgets');
 });
 
 // add assignment dialog box
--- mygrades/static/javascript/semester-overview.js
+++ mygrades/static/javascript/semester-overview.js
@@ -1,3 +1,50 @@
-/**
- * Created by nick on 10/20/13.
- */
+// draggable sorting of the course list
+$(function() {
+    var sort = $('#sortable-courselist');
+    sort.sortable({
+      	placeholder: 'course placeholder'
+    });
+    sort.disableSelection();
+});
+
+// add course dialog box
+$(function() {
+	var number = $( "#number" );
+	var name = $( "#name" );
+	var instructor = $( "#instructor" );
+	//var allFields = $( [] ).add( number ).add( name ).add( instructor );
+	var tips = $( ".validate-tips" );
+
+	$( "#add-course-dialog-form" ).dialog({
+		autoOpen: false,
+		height: 325,
+		width: 375,
+		modal: true,
+		buttons: {
+			"Add course": function() {
+				var isValid = true;
+
+				// do stuff to check for validity of inputs
+
+		        if ( isValid ) {
+	      			// store data in database
+	      			$( this ).dialog( "close" );
+	    	  	} else {
+	    	  		// change validate-tips to help user type valid inputs
+	    	  	}
+	      	},
+		  	Cancel: function() {
+	  			$( this ).dialog( "close" );
+		  	}
+		},
+		close: function() {
+			// do special stuff when closing window
+			// such as prep for next time window is opened
+		}
+	});
+
+	$( ".add-course-button" ).button().click(function() {
+		$( "#add-course-dialog-form" ).dialog( "open" );
+	});
+});
+
