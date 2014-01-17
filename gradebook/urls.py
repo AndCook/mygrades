a96821bad8e5d3b4b97a1891595adc5da7741e11
Nick Walther <newalth@cs.utexas.edu>
2014-01-17 16:56:52 -0600

    Current Courses and Categories table pages added

--- gradebook/urls.py
+++ gradebook/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('gradebook.views',
                        url(r'^overview/$', 'overview'),
+                       url(r'^current_courses/$', 'current_courses'),
                        url(r'^course_detail/(?P<course_id>\w+)/$', 'course_detail'),
 )
