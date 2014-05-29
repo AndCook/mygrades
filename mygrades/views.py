3b8103e478371330cb23b693aebe98f202b44f71
Nick Walther <newalth@cs.utexas.edu>
2014-05-28 20:40:36 -0700

    Trying out mobile detection

--- mygrades/views.py
+++ mygrades/views.py
@@ -7,9 +7,42 @@ from django.core.mail import EmailMessage
 from mygrades.settings import EMAIL_HOST_USER
 
 
+# list of mobile User Agents
+mobile_uas = ['w3c ', 'acs-', 'alav', 'alca', 'amoi', 'audi', 'avan', 'benq', 'bird', 'blac',
+              'blaz', 'brew', 'cell', 'cldc', 'cmd-', 'dang', 'doco', 'eric', 'hipt', 'inno',
+              'ipaq', 'java', 'jigs', 'kddi', 'keji', 'leno', 'lg-c', 'lg-d', 'lg-g', 'lge-',
+              'maui', 'maxo', 'midp', 'mits', 'mmef', 'mobi', 'mot-', 'moto', 'mwbp', 'nec-',
+              'newt', 'noki', 'oper', 'palm', 'pana', 'pant', 'phil', 'play', 'port', 'prox',
+              'qwap', 'sage', 'sams', 'sany', 'sch-', 'sec-', 'send', 'seri', 'sgh-', 'shar',
+              'sie-', 'siem', 'smal', 'smar', 'sony', 'sph-', 'symb', 't-mo', 'teli', 'tim-',
+              'tosh', 'tsm-', 'upg1', 'upsi', 'vk-v', 'voda', 'wap-', 'wapa', 'wapi', 'wapp',
+              'wapr', 'webc', 'winw', 'winw', 'xda', 'xda-']
+
+mobile_ua_hints = ['SymbianOS', 'Opera Mini', 'iPhone']
+
+
+def mobile_browser(request):
+    # Super simple device detection, returns True for mobile devices
+
+    is_mobile = False
+    ua = request.META['HTTP_USER_AGENT'].lower()[0:4]
+
+    if ua in mobile_uas:
+        is_mobile = True
+    else:
+        for hint in mobile_ua_hints:
+            if request.META['HTTP_USER_AGENT'].find(hint) > 0:
+                is_mobile = True
+
+    return is_mobile
+
+
 def home(request):
-    return render_to_response('home.html',
-                              RequestContext(request))
+    if mobile_browser(request):
+        template = 'm_home.html'
+    else:
+        template = 'home.html'
+    return render_to_response(template, RequestContext(request))
 
 
 def about(request):
